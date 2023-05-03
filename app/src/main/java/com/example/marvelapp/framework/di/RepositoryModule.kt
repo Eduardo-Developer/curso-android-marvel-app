1e9b85d1b6f617dc6ac73c9a3d9b9727459b9da4
Cadu <carlos.souza@maximatech.com.br>
2023-05-03 17:04:14 -0300

    Implementa characters paging source.

---
+++ app/src/main/java/com/example/marvelapp/framework/di/RepositoryModule.kt
@@ -0,0 +1,26 @@
+package com.example.marvelapp.framework.di
+
+import com.edudev.core.data.repository.CharacterRepository
+import com.edudev.core.data.repository.CharactersRemoteDataSource
+import com.example.marvelapp.framework.CharacterRepositoryImpl
+import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import com.example.marvelapp.framework.remote.RetrofitCharactersDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+
+    @Binds
+    fun bindCharacterRepository(
+        repository: CharacterRepositoryImpl
+    ) : CharacterRepository
+
+    @Binds
+    fun bindRemoteDataSource(
+        dataSource: RetrofitCharactersDataSource
+    ) : CharactersRemoteDataSource<DataWrapperResponse>
+}
